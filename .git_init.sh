1a19e9fb33336de352678de915718c12487b947c
Mike <mibamur@gmail.com>
2013-08-02 01:33:03 +1000

    create dev branch

--- .git_init.sh
+++ .git_init.sh
@@ -1,6 +1,8 @@
-git init
-touch README
+#!/bin/bash
+
+te=$1
+
 git add .
-git commit -m 'first commit'
-git remote add origin git@git.i.i:root/vtigercrm6.git
-git push -u origin master
+git add -A
+git commit -m "$te"
+git push
